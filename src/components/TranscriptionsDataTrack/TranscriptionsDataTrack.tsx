85a3928795eaada0e8053d0dbf8564c4e432dd0e
Arjun Sasikumar <asasikumar@twilio.com>
2022-03-17 14:49:15 -0700

    parse aws transcript response

--- src/components/TranscriptionsDataTrack/TranscriptionsDataTrack.tsx
+++ src/components/TranscriptionsDataTrack/TranscriptionsDataTrack.tsx
@@ -16,20 +16,27 @@ export default function TranscriptionsDataTrack({ track }: { track: any }) {
 
   const onMessage = useCallback(
     (message: string) => {
-      const transcription = JSON.parse(message) as TranscriptionMessage;
+      const transcription = JSON.parse(message);
+      if (transcription.transcriptionResponse.TranscriptEvent.Transcript.Results.length) {
+        const isFinal = transcription.transcriptionResponse.TranscriptEvent.Transcript.Results[0].isPartial;
+        const transcript =
+          transcription.transcriptionResponse.TranscriptEvent.Transcript.Results[0].Alternatives[0].Transcript;
 
-      addMessages({
-        messageId: transcription.messageId,
-        author: transcription.identity,
-        sid: `${new Date().getTime()}`,
-        dateCreated: new Date(),
-        body: transcription.data.transcript,
-        isFinal: transcription.data.isFinal,
-      });
+        addMessages({
+          messageId: transcription.messageId,
+          author: transcription.identity,
+          sid: `${new Date().getTime()}`,
+          dateCreated: new Date(),
+          body: transcript,
+          isFinal,
+        });
+      }
     },
     [addMessages]
   );
-
+  /*
+    {"transcriptionResponse":{"TranscriptEvent":{"Transcript":{"Results":[{"Alternatives":[{"Items":[{"Content":"Hello","EndTime":1.8725,"StartTime":1.2875,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":1.8725,"StartTime":1.8725,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":2.7625,"StartTime":1.8725,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":2.7625,"StartTime":2.7625,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":3.6625,"StartTime":2.7625,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":3.6625,"StartTime":3.6625,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":4.4375,"StartTime":3.6625,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":4.4375,"StartTime":4.4375,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":5.4575,"StartTime":5.1275,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":5.4575,"StartTime":5.4575,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":6.0925,"StartTime":5.4575,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":6.0925,"StartTime":6.0925,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Hello","EndTime":6.5775,"StartTime":6.0925,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":"?","EndTime":6.5775,"StartTime":6.5775,"Type":"punctuation","VocabularyFilterMatch":false},{"Content":"Yes","EndTime":7.7675,"StartTime":7.4075,"Type":"pronunciation","VocabularyFilterMatch":false},{"Content":".","EndTime":7.7675,"StartTime":7.7675,"Type":"punctuation","VocabularyFilterMatch":false}],"Transcript":"Hello? Hello? Hello? Hello? Hello? Hello? Hello? Yes."}],"EndTime":7.915,"IsPartial":true,"ResultId":"56e434f4-db19-490d-9cd8-ab382cd6e9b5","StartTime":0.9}]}}},"participantIdentity":"Arjun"}
+    */
   useEffect(() => {
     // NOTE: instead of pushing to the provider the messages, we should
     // find the participant which has the localDataTrack and subscribe it there
